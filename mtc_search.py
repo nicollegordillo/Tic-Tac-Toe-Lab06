65033490f9670b0a92604a70f1c9dab53ef223ea
MarchMol <marchmol98@gmail.com>
2025-04-29 19:10:29 -0600

    fix: divide by 0 problem and added experiment function

--- mtc_search.py
+++ mtc_search.py
@@ -1,25 +1,30 @@
 import random
-
 import numpy as np
+import pandas as pd
 from tic_tac_toe import TicTacToe
-import copy
+import matplotlib.pyplot as plt
 class Mtc_Node:
-    def __init__(self, state, start_player, move):
+    def __init__(self, state, start_player, move, c):
         self.parent = None
         self.state = state
         self.move = move
         self.moves = self.state.available_moves()
         self.children = []
         self.player = start_player
+        
+        self.c = c
         # Simulations with wins
         self.wi = 0
         # Total simulations
         self.si = 0
     
-    def ucb(self, child, c=1.41):
-        return(
-            (child.wi/child.si) + c*np.sqrt(np.log(self.si)/child.si)
-        )
+    def ucb(self, child):
+        if child.si == 0:
+            return float("inf")
+        else:
+            return(
+                (child.wi/child.si) + self.c*np.sqrt(np.log(self.si)/child.si)
+            )
     def best_child(self):
         q = float("-inf")
         best = None
@@ -36,14 +41,13 @@ class Mtc_Node:
         new_state.make_move(move, 
             'X' if not self.player else 'O'
         )
-        child = Mtc_Node(new_state, not self.player, move)
+        child = Mtc_Node(new_state, not self.player, move, self.c)
         self.children.append(child)
         return child
-def mtc(_root, n_sims, player):
-    root = Mtc_Node(_root, player, None)
-    
-    
-    for i in range(3):
+def mtc(_root, n_sims, player, c):
+    root = Mtc_Node(_root, player, None, c)
+    root.si = 1
+    for _ in range(n_sims):
         node: Mtc_Node = root
         # 1. Selection
         while(
@@ -79,19 +83,47 @@ def mtc(_root, n_sims, player):
         # 5. Return most visited
     return max(root.children, key=lambda c: c.si).move
 
-def play_mtc(n_sims):
+def play_mtc(n_sims, c):
     game = TicTacToe()
-    player = 'X'
+    player = random.choice(['X','O'])
     
     while not game.is_terminal():
         if player =='X':
-            move = mtc(game, n_sims, True)
+            move = mtc(game, n_sims, True, c)
         else:
             move = random.choice(game.available_moves())
         game.make_move(move, player)
         player = 'O' if player == 'X' else 'X'
-        game.print_board()
-        print("\n")
+    return game.current_winner()
     
-    
-play_mtc(5)
+def experiment(n_sim, c, N):
+    data = {"winner": []}
+    for _ in range(N):
+        t = play_mtc(n_sim, c)
+        if t:
+            data["winner"].append(t)
+        else: 
+            data["winner"].append("tie")
+    df = pd.DataFrame(data)
+    counts = df["winner"].value_counts()
+    return counts
+
+def value_tune():
+    N = 300
+    sim_opt = [10, 50, 100, 200, 500]
+    c_opt = [0.1, 0.5, 1.0, 1.41, 2.0, 3.0, 5.0]
+    matrix = np.zeros((len(c_opt),len(sim_opt)))
+    for i,c in enumerate(c_opt):
+        for j,n in enumerate(sim_opt):
+            rslt = experiment(n, c, N)["X"] / N
+            matrix[i][j] = rslt
+    plt.imshow(matrix, cmap='viridis', vmin=matrix.min(), vmax=matrix.max())
+    plt.xticks(ticks=np.arange(len(sim_opt)), labels=sim_opt)
+    plt.yticks(ticks=np.arange(len(c_opt)), labels=c_opt)
+    plt.xlabel("Number of Simulations")
+    plt.ylabel("C Value")
+    plt.colorbar(label='Value')
+    plt.title("MTC Win Percentage")
+    plt.show()
+
+value_tune()
